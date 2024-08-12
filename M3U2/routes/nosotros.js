3516a1c6beae423c7e7eb03c86992635e29eeef0
Maxi Guillamon <guillamonmaxii@gmail.com>
2024-08-11 22:27:31 -0300

    commit

---
+++ M3U2/routes/nosotros.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req ,res ,next){
+    res.send('hola soy la pagina nosotros');
+})
+module.exports = router;
